d6d0a64d0e74a252080bf4ea55c0ec10c3479e15
Shivshankar Sahoo <sahoo.shivshankar@gmail.com>
2022-10-13 23:59:14 +0530

    Added string basics

--- 04-js-basics-revamp/script.js
+++ 04-js-basics-revamp/script.js
@@ -17,12 +17,12 @@ var num = 98;
 var hugeInt = BigInt(9007199254740991);
 var sym = Symbol('foo');
 
-console.log(typeof myName)              // prints string
-console.log(typeof nullObj)             // prints object
-console.log(typeof undefObj)            // prints undefined
-console.log(typeof num)                 // prints number
-console.log(typeof hugeInt)             // prints bigint
-console.log(typeof sym)                 // prints symbol
+console.log(typeof myName);             // prints string
+console.log(typeof nullObj);            // prints object
+console.log(typeof undefObj);           // prints undefined
+console.log(typeof num);                // prints number
+console.log(typeof hugeInt);            // prints bigint
+console.log(typeof sym);                // prints symbol
 
 /* 
 5] One of the biggest problems with declaring variables with the 'var' keyword is that you can
@@ -43,4 +43,41 @@ variables declared using 'const' are read-only.
 They are a constant value, which means that once a variable is assigned with 'const', it cannot be reassigned.
 */
 const FAV_PET = "Cats";
-FAV_PET = "Dogs";                       // gives error: Uncaught TypeError: Assignment to constant variable.
+//FAV_PET = "Dogs";                     // gives error: Uncaught TypeError: Assignment to constant variable.
+console.log(FAV_PET);
+
+/*
+7] String values in JavaScript may be written with single or double quotes,
+as long as you start and end with the same type of quote. Unlike some other programming languages,
+single and double quotes work the same in JavaScript.
+*/
+const doubleQuoteStr = "This is a string";
+console.log(doubleQuoteStr);
+const singleQuoteStr = 'This is also a string';
+console.log(singleQuoteStr);
+
+/*
+8] Bracket notation is a way to get a character at a specific index within a string.
+Most modern programming languages, like JavaScript, don't start counting at 1 like humans do.
+They start at 0. This is referred to as Zero-based indexing.
+*/
+const firstName = "Charles";
+const firstLetter = firstName[0];
+console.log("firstLetter: " + firstLetter);
+
+/*
+9] In JavaScript, String values are immutable, which means that they cannot be altered once created.
+For example, the following code will produce an error because the letter B in the string Bob cannot be
+changed to the letter J:
+*/
+let myStr = "Bob";
+myStr[0] = "J";
+console.log("myStr: " + myStr)
+
+/*
+Note that this does not mean that myStr could not be re-assigned.
+The only way to change myStr would be to assign it with a new value, like this:
+*/
+let myStrr = "Bob";
+myStrr = "Job";
+console.log("myStrr: " + myStrr);
